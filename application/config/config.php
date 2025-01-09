93d9ebc670e410214168431cf29890c32ec31208
NurjamanST <168767148+NurjamanST@users.noreply.github.com>
2025-01-10 02:30:11 +0700

    Update Config

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ setlocale(LC_ALL, 'IND');
 |
 */
 // $config['base_url'] = "https://meniti.id/cbtaktara";
-$config['base_url'] = 'https://localhost/CBTAKTARA/';
+$config['base_url'] = 'https://meniti.id/CBTAKTARA/';
 // $config['base_url'] = '';
 // $config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 // $config['base_url'] = str_replace('index.php', '', $config['base_url']);
